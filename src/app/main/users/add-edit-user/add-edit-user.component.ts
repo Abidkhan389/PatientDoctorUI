7c64e291c0cee1ffcec76cb479e0c23081161487
abidkhan <Abidullah.khan@Khazanapk.com>
2023-10-28 11:30:11 +0500

    changes

--- src/app/main/users/add-edit-user/add-edit-user.component.ts
+++ src/app/main/users/add-edit-user/add-edit-user.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup,   } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Messages } from 'src/app/_common';
+import { UserManagementService } from 'src/app/_services/administration/user-management.service';
 
 @Component({
   selector: 'app-add-edit-user',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-edit-user.component.sass']
 })
 export class AddEditUserComponent {
+  
+  isreadOnly: boolean = false;
+  CourseForm: FormGroup;
+  validationMessages = Messages.validation_messages;
+  userList:any;
+  constructor(public userManagementService: UserManagementService,private fb: FormBuilder, protected router: Router, private dialogref: MatDialogRef<AddEditUserComponent>,
+    private dilog: MatDialog, @Inject(MAT_DIALOG_DATA) public data: any) {
+    
+  }
+  ngOnInit(): void {
+    if (this.data.courseId) {
+      this.GetUser()
+    }
+    this.validateform();
+  }
+  GetUser(){
 
+  }
+  validateform(){
+    
+  }
 }
